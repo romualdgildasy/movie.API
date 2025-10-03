17f546e9dd0d90750301884b1bf49b3f61d535ee
Romuald <romualdgilads6@gmail.com>
2025-10-03 19:28:09 +0100

    creation d'un DTo pour stocker les filmes

---
+++ Dtos/MovieDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace movie.API.Dtos;
+
+public class MovieDto
+(
+    int Id,
+    string Title,
+    string Director,
+    DateOnly releaseDate
+    
+);
---
+++ Program.cs
@@ -0,0 +1,18 @@
+using movie.API.Dtos;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<MovieDto> movies =
+[
+    new(1,"Inception","Christopher Nolan", new DateOnly(2010,7,16)),
+    new(2,"Matrix","Toukof Lerusse", new DateOnly(1993,3,31)),
+    new(3,"Le Parrain","Francis Ford", new DateOnly(1972,3,24)),
+];
+
+//route pour nos film
+app.MapGet("/movies", () => movies);
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
