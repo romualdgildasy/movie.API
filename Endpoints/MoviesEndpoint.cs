60b319e8ac81dc736b4626fbb267e8d11d5607a3
Romuald <romualdgilads6@gmail.com>
2025-10-09 15:34:20 +0100

    changement des prefice app en group pour facllité la compatibiloté vu que c'est e group qu'on a mappé dans le program.cs

--- Endpoints/MoviesEndpoint.cs
+++ Endpoints/MoviesEndpoint.cs
@@ -2,7 +2,59 @@ using System;
 
 namespace movie.API.endpoints;
 
-public class MoviesEndpoint
+public static class MoviesEndpoint
 {
+     public static RouterGroupBuilder MapMoviesEndpoint (this WebApplication app)
+     {
+        var group = app.MapGroup("/movies")
 
+        group.MapGet("/", () => movies);
+
+                group.MapGet("/{id}", (int id) =>
+                {
+                    MovieDto ? movie = movies.Find(movie => movie.Id == id);
+                    return (movie is null) ? Results.NotFound() : Results.Ok(movie);
+                }).WithName("GetMovie");
+            //creation de la route de post
+                group.MapPost("/", (CreateMovieDto newMovie) =>
+                {
+                    MovieDto movie = new
+                        (
+                        Id: movies.Count + 1,
+                        Title: newMovie.Title,
+                        Director: newMovie.Director,
+                        ReleaseDate: newMovie.ReleaseDate
+                        );
+                    movies.Add(movie);
+
+                    return Results.CreatedAtRoute("GetMovie", new { id = movie.Id }, movie);
+                });
+
+            //creatio de la methgo PUT  pour modifier les films
+                group.MapPut("/{id}", (int id, UpdateMovieDto updateMovie) =>
+                    {
+                        var index = movies.FindIndex(movie => movie.Id == id);
+                        if (index== -1)
+                        {
+                            return Results.NotFound();
+                        }
+                        movies[index] = new MovieDto
+                        (
+                            id,
+                            updateMovie.Title,
+                            updateMovie.Director,
+                            updateMovie.ReleaseDate
+                        );
+                        return Results.NoContent();
+                    }
+                );
+
+            //creation de la methode de suppression
+                group.MapDelete("/{id}", (int id) =>
+                {
+                    movies.RemoveAll(movie => movie.Id == id);
+                    return Results.NoContent();
+                });
+
+    }
 }
--- Program.cs
+++ Program.cs
@@ -11,52 +11,5 @@ List<MovieDto> movies =
 ];
 
 //route pour nos film
-app.MapGet("movies", () => movies);
-
-app.MapGet("movies{id}", (int id) =>
-{
-    MovieDto ? movie = movies.Find(movie => movie.Id == id);
-    return (movie is null) ? Results.NotFound() : Results.Ok(movie);
-}).WithName("GetMovie");
-//creation de la route de post
-app.MapPost("movies", (CreateMovieDto newMovie) =>
-{
-    MovieDto movie = new
-        (
-        Id: movies.Count + 1,
-        Title: newMovie.Title,
-        Director: newMovie.Director,
-        ReleaseDate: newMovie.ReleaseDate
-        );
-    movies.Add(movie);
-
-    return Results.CreatedAtRoute("GetMovie", new { id = movie.Id }, movie);
-});
-
-//creatio de la methgo PUT  pour modifier les films
-app.MapPut("movies/{id}", (int id, UpdateMovieDto updateMovie) =>
-    {
-        var index = movies.FindIndex(movie => movie.Id == id);
-        if (index== -1)
-        {
-            return Results.NotFound();
-        }
-        movies[index] = new MovieDto
-        (
-            id,
-             updateMovie.Title,
-             updateMovie.Director,
-             updateMovie.ReleaseDate
-        );
-        return Results.NoContent();
-    }
-);
-
-//creation de la methode de suppression
-app.MapDelete("movies/{id}", (int id) =>
-{
-    movies.RemoveAll(movie => movie.Id == id);
-    return Results.NoContent();
-});
 
 app.Run();
